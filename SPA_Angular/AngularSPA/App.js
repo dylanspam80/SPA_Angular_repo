ddc0b2ff616c9d27233b2c2362a4b90b4fa5f1ac
dylansmallms@gmail.com <dylansmallms@gmail.com>
2020-02-06 22:22:21 +0200

    Added angular controllers

--- SPA_Angular/AngularSPA/App.js
+++ SPA_Angular/AngularSPA/App.js
@@ -1,3 +1,3 @@
 ﻿var App = angular.module('App', ['ngRoute']);
-
+App.service('Api', ['$http', ApiServices]);
 App.controller('LoginController', LoginController);
--- SPA_Angular/AngularSPA/Controllers/LoginController.js
+++ SPA_Angular/AngularSPA/Controllers/LoginController.js
@@ -1,4 +1,16 @@
 ﻿var LoginController = function ($scope) {
-
+    $scope.models = {
+        CurrentUser: {
+            Username: "",
+            Password: ""
+        }
+    };
 }
+
+function GetUser() {
+    Api.Get("Locations", "GetLocations", function (event) {
+        $scope.models.CurrentUser = event.result;
+    });
+}
+
 LoginController.$inject = ['$scope'];
