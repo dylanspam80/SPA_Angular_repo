16fa32e5b0ac1a0d88f68761f594978fd618cbe4
dylansmallms@gmail.com <dylansmallms@gmail.com>
2020-02-06 22:13:00 +0200

    Changed method names

--- SPA_Angular/AngularSPA/ApiServices.js
+++ SPA_Angular/AngularSPA/ApiServices.js
@@ -1,6 +1,6 @@
 ﻿var ApiService = function ($http) {
     var result;
-    this.GetApiCall = function (controllerName, method, callback) {
+    this.Get = function (controllerName, method, callback) {
         result = $http.get('api/' + controllerName + '/' + method).success(
             function (data, status) {
                 var event = {
@@ -20,7 +20,7 @@
             );
     }
 
-    this.PostApiCall = function (controllerName, methodName, obj, callback) {
+    this.Post = function (controllerName, methodName, obj, callback) {
         result = $http.post('api/' + controllerName + '/' + methodName, obj).success(function (data, status) {
             var event = {
                 result: data,
