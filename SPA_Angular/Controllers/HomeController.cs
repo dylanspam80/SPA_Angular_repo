fafa07e5e15b976d40358faf55f494416dae3bb2
dylansmallms@gmail.com <dylansmallms@gmail.com>
2020-02-07 04:59:56 +0200

    nav

--- SPA_Angular/Controllers/HomeController.cs
+++ SPA_Angular/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace SPA_Angular.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
